dec81590047bc4243c3de6018bd9644dd2908327
zzsanduo <zzsanduo@gmail.com>
2016-03-29 21:46:41 +0800

    running context

--- app/index.js
+++ app/index.js
@@ -4,8 +4,26 @@ var generators = require('yeoman-generator');
 
 
 module.exports = generators.Base.extend({
-    test:function(){
-        this.log('hello,world');
+    initializing:function(){
+        this.log('initializing!');
+    },
+    prompting:function(){
+        this.log('prompting');
+    },
+    configuring:function(){
+        this.log('configuring');
+    },
+    default:function(){
+        this.log('writing');
+    },
+    writing:function(){
+        this.log('writing');
+    },
+    conflicts:function(){
+        this.log('install');
+    },
+    end:function(){
+        this.log('end');
     }
 })
 
