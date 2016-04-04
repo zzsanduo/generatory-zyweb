c7f029acc23ec8b5e9bb59fd73a25e2883382937
zzsanduo <zzsanduo@gmail.com>
2016-04-04 16:54:00 +0800

    copy favicon.ico, index.html and main.css

--- app/index.js
+++ app/index.js
@@ -16,8 +16,17 @@ module.exports = generators.Base.extend({
     default:function(){
         this.log('writing');
     },
-    writing:function(){
-        this.log('writing');
+    writing:{
+        appStaticFiles:function(){
+            this.copy('_favicon.ico',"favicon.ico");
+            this.directory('styles','styles/')
+        },
+        html:function(){
+            this.fs.copyTpl(
+                this.templatePath('_index.html'),
+                this.destinationPath('index.html')
+            );
+        }
     },
     conflicts:function(){
         this.log('install');
